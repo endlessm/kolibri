c3c2f5b1dcde2e9f41c6f7a66c28185e2447ea31
Manuel Quiñones <manuel.por.aca@gmail.com>
2020-08-10 15:56:06 -0300

    html5_viewer plugin: Provide API to custom channel presentation
    The webapp inside the iframe is able to call the following methods
    through window.parent.postMessage:
    
    - askChannelInformation: Retrieves all the tree-like structure for the
      channel, excluding the current node (the one with the custom channel
      presentation). Also, turn fullscreen on.
    
    - goToContent: Navigate to the node with ID passed as parameter. Also,
      turn fullscreen off.
    
    For now, all thumbnails are sent at once as binary data, along with
    the channel information.
    
    https://phabricator.endlessm.com/T30628

--- kolibri/plugins/html5_viewer/assets/src/views/Html5AppRendererIndex.vue
+++ kolibri/plugins/html5_viewer/assets/src/views/Html5AppRendererIndex.vue
@@ -53,6 +53,9 @@
   import Hashi from 'hashi';
   import { nameSpace } from 'hashi/src/hashiBase';
   import plugin_data from 'plugin_data';
+  import { ContentNodeResource } from 'kolibri.resources';
+  import { getContentNodeThumbnail } from 'kolibri.utils.contentNode';
+  import axios from 'axios';
 
   // Regex vendored from https://github.com/faisalman/ua-parser-js/blob/master/src/ua-parser.js
   const iOSTest = /ip[honead]{2,4}(?:.*os\s([\w]+)\slike\smac|;\sopera)/i;
@@ -126,6 +129,24 @@
       },
     },
     mounted() {
+      window.addEventListener('message', event => {
+        if (!event.data.event || !event.data.nameSpace || event.data.nameSpace !== 'customChannelPresentation') {
+          return;
+        }
+        if (event.data.event === 'askChannelInformation') {
+          if (!this.isInFullscreen) {
+            this.$refs.html5Renderer.toggleFullscreen();
+          }
+          this.sendChannelInformation();
+        }
+        if (event.data.event === 'goToContent') {
+          if (this.isInFullscreen) {
+            this.$refs.html5Renderer.toggleFullscreen();
+          }
+          this.goToContent(event.data.data);
+        }
+      });
+
       this.hashi = new Hashi({ iframe: this.$refs.iframe, now });
       this.hashi.onStateUpdate(data => {
         this.$emit('updateContentState', data);
@@ -145,6 +166,51 @@
       this.$emit('stopTracking');
     },
     methods: {
+      sendChannelInformation() {
+        if (!this.$refs.iframe) {
+          return;
+        }
+        const iframeWindow = this.$refs.iframe.contentWindow;
+        const channel = this.$store.state.topicsTree.channel;
+        const currentNodeId = this.$store.state.topicsTree.content.id;
+
+        ContentNodeResource.fetchCollection({
+          getParams: {
+            channel_id: channel.id,
+            user_kind: this.$store.getters.getUserKind,
+          },
+        }).then((nodes) => {
+          const promises = nodes.filter((node) => {
+            return node.id !== currentNodeId;
+          }).map((node) => {
+            const thumbnailUrl = getContentNodeThumbnail(node);
+            if (!thumbnailUrl) {
+              return node;
+            }
+            const promise = axios.get(thumbnailUrl, {responseType: 'arraybuffer'}).then((response) => {
+              const returnedB64 = Buffer.from(response.data).toString('base64');
+              node.thumbnail = "data:" + response.headers["content-type"] + ";base64," + returnedB64;
+              return node;
+            });
+            return promise;
+          });
+
+          Promise.all(promises).then((nodes) => {
+            const event = 'sendChannelInformation';
+            const message = {
+              event,
+              nameSpace,
+              data: {channel, nodes},
+            };
+            iframeWindow.postMessage(message, '*');
+          });
+        });
+      },
+
+      goToContent(id) {
+        this.$router.push({ name: 'TOPICS_CONTENT', params: { id }});
+      },
+
       recordProgress() {
         const totalTime = now() - this.startTime;
         const hashiProgress = this.hashi ? this.hashi.getProgress() : null;
