0e3e8f889f0c737685ec08c3b53db7eb99e10347
Manuel Quiñones <manuel.por.aca@gmail.com>
2020-12-29 11:48:33 -0300

    WIP, custom presentation component in explore

---
+++ kolibri/plugins/explore/assets/src/views/CustomChannelPresentationApp.vue
@@ -0,0 +1,111 @@
+<template>
+      <iframe
+        ref="iframe"
+        class="iframe"
+        :style="{ backgroundColor: 'blue', height: '600px', width: '100%' }"
+        frameBorder="0"
+        :src="rooturl"
+      >
+      </iframe>
+</template>
+
+<script>
+
+  import { nameSpace } from 'hashi/src/hashiBase';
+  import { ContentNodeResource } from 'kolibri.resources';
+  import { getContentNodeThumbnail } from 'kolibri.utils.contentNode';
+  import axios from 'axios';
+
+  export default {
+    name: 'CustomChannelPresentationApp',
+    computed: {
+      rooturl() {
+        return '/zipcontent/eb6616e4c27666e0a066ae0d7734871d.zip/' + '?SKIP_HASHI=true' + '?date=' + (+ new Date());
+        // return this.defaultFile.storage_url + '?SKIP_HASHI=true' + '?date=' + (+ new Date());
+      },
+    },
+    mounted() {
+      window.addEventListener('message', event => {
+        if (!event.data.event || !event.data.nameSpace || event.data.nameSpace !== 'customChannelPresentation') {
+          return;
+        }
+        if (event.data.event === 'askChannelInformation') {
+          this.sendChannelInformation();
+        }
+        if (event.data.event === 'goToContent') {
+          this.goToContent(event.data.data);
+        }
+        if (event.data.event === 'getThumbnail') {
+          this.sendThumbnail(event.data.data);
+        }
+      });
+    },
+    methods: {
+      sendChannelInformation() {
+        if (!this.$refs.iframe) {
+          return;
+        }
+        const iframeWindow = this.$refs.iframe.contentWindow;
+        const channel = this.$store.state.topicsTree.channel;
+        const currentNodeId = this.$store.state.topicsTree.content.id;
+
+        ContentNodeResource.fetchCollection({
+          getParams: {
+            channel_id: channel.id,
+            user_kind: this.$store.getters.getUserKind,
+          },
+        }).then((nodes) => {
+          const promises = nodes.filter((node) => {
+            return node.id !== currentNodeId;
+          });
+
+          Promise.all(promises).then((nodes) => {
+            const event = 'sendChannelInformation';
+            const message = {
+              event,
+              nameSpace,
+              data: {channel, nodes},
+            };
+            iframeWindow.postMessage(message, '*');
+          });
+        });
+      },
+
+      sendThumbnail(id) {
+        const iframeWindow = this.$refs.iframe.contentWindow;
+        const event = 'sendThumbnail';
+        ContentNodeResource.fetchModel({ id }).then((node) => {
+          const thumbnailUrl = getContentNodeThumbnail(node);
+          if (!thumbnailUrl) {
+            const message = {
+              event,
+              nameSpace,
+              data: {id, thumbnail: null},
+            };
+            iframeWindow.postMessage(message, '*');
+            return;
+          }
+          const promise = axios.get(thumbnailUrl, {responseType: 'arraybuffer'}).then((response) => {
+              const returnedB64 = Buffer.from(response.data).toString('base64');
+              const thumbnail = "data:" + response.headers["content-type"] + ";base64," + returnedB64;
+              return thumbnail;
+          });
+          promise.then((thumbnail) => {
+            const message = {
+              event,
+              nameSpace,
+              data: {id, thumbnail},
+            };
+            iframeWindow.postMessage(message, '*');
+          });
+        });
+      },
+
+      goToContent(id) {
+        this.$router.push({ name: 'TOPICS_CONTENT', params: { id }});
+      },
+
+    },
+  };
+
+</script>
